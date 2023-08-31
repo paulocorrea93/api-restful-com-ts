0ae5a2080b697725c5703c9698bc669f61161ccd
Paulo <paulocorreapc93@gmail.com>
2023-08-31 11:59:17 -0400

    criação de controllers

---
+++ src/controllers/movieControllers.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+import { MovieModel } from "../models/Movie";
+
+import Logger from "../../config/logger";
+
+export async function createMovie(req: Request, res: Response) {
+    return res.status(200).send("Deu certo o controller.")
+}
