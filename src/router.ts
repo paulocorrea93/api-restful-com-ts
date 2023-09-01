ac53a6a43fced596e5c835229276dd8b01ee8fe3
Paulo <paulocorreapc93@gmail.com>
2023-09-01 17:36:34 -0400

    atualizando registros

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import {
   findMovieById,
   getAllMovies,
   removeMovie,
+  updateMovie,
 } from "./controllers/movieControllers";
 
 import { validate } from "./middleware/handleValidation";
@@ -18,4 +19,5 @@ export default router
   .post("/movie", movieCreateValidation(), validate, createMovie)
   .get("/movie/:id", findMovieById)
   .get("/movie", getAllMovies)
-  .delete("/movie/:id", removeMovie);
+  .delete("/movie/:id", removeMovie)
+  .patch("/movie/:id", updateMovie);
