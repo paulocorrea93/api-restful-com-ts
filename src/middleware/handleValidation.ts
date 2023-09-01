843ac2ea4ee837027645516ed5755eb3781fba49
Paulo <paulocorreapc93@gmail.com>
2023-09-01 14:20:50 -0400

    middleware de validaçao de erros

---
+++ src/middleware/handleValidation.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  const extratedErrors: object[] = [];
+
+  errors.array().map((err) => extratedErrors.push({ [err.msg]: err.msg }));
+
+  return res.status(422).json({
+    errors: extratedErrors,
+  });
+};
